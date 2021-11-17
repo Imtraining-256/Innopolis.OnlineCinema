37b81a19d43927ea9c16c29c113df1022fa0e7ed
atenyaeves <atenyaev@protonmail.com>
2021-11-17 18:52:06 +0700

    Fix method name

--- app/src/main/java/com/example/innopolisonlinecinema/data/api/MoviesRemoteSource.kt
+++ app/src/main/java/com/example/innopolisonlinecinema/data/api/MoviesRemoteSource.kt
@@ -3,7 +3,7 @@ package com.example.innopolisonlinecinema.data.api
 import com.example.innopolisonlinecinema.data.api.model.Results
 
 class MoviesRemoteSource(private val api: MoviesApi) {
-    suspend fun fetchMovies(): Results {
+    suspend fun getMovies(): Results {
         return api.getMovies()
     }
 }
