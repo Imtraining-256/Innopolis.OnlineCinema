302c69211abbd265eaa3fbe83342a2bef2ffdb78
atenyaeves <atenyaev@protonmail.com>
2021-11-17 17:11:20 +0700

    Added result and list result models

---
+++ app/src/main/java/com/example/innopolisonlinecinema/data/api/model/Result.kt
@@ -0,0 +1,25 @@
+package com.example.innopolisonlinecinema.data.api.model
+
+import com.google.gson.annotations.SerializedName
+
+/**
+ * A data class for film
+ */
+data class Result(
+    @SerializedName("genres")
+    val genres: List<Genre>,
+    @SerializedName("original_title")
+    val originalTitle: String,
+    @SerializedName("overview")
+    val overview: String,
+    @SerializedName("release_date")
+    val releaseDate: String,
+    @SerializedName("poster_path")
+    val posterPath: String,
+    @SerializedName("title")
+    val title: String,
+    @SerializedName("video")
+    val video: String,
+    @SerializedName("vote_average")
+    val voteAverage: Double
+)
