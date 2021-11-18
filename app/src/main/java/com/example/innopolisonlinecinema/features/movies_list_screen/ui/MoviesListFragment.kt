617a986a698292ab5e2c9e3f11c72a173afeea80
atenyaeves <atenyaev@protonmail.com>
2021-11-18 11:54:28 +0700

    Added fragment

---
+++ app/src/main/java/com/example/innopolisonlinecinema/features/movies_list_screen/ui/MoviesListFragment.kt
@@ -0,0 +1,10 @@
+package com.example.innopolisonlinecinema.features.movies_list_screen.ui
+
+import androidx.fragment.app.Fragment
+import com.example.innopolisonlinecinema.R
+
+class MoviesListFragment : Fragment(R.layout.fragment_movies_list) {
+    companion object {
+        fun newInstance() = MoviesListFragment()
+    }
+}
