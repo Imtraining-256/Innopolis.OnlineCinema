0ef5da884d1cc9e6bfa344284111b763f2ead67e
atenyaeves <atenyaev@protonmail.com>
2021-11-23 17:55:10 +0700

    Added VM

---
+++ app/src/main/java/com/example/innopolisonlinecinema/features/movies_list_screen/ui/MoviesListViewModel.kt
@@ -0,0 +1,41 @@
+package com.example.innopolisonlinecinema.features.movies_list_screen.ui
+
+import com.example.innopolisonlinecinema.base.BaseViewModel
+import com.example.innopolisonlinecinema.base.Event
+import com.example.innopolisonlinecinema.domain.MovieInteractor
+
+class MoviesListViewModel(private val interactor: MovieInteractor) : BaseViewModel<ViewState>() {
+    override fun initialViewState(): ViewState {
+        return ViewState(movies = listOf(), errorMessage = null, isLoad = false)
+    }
+
+    override suspend fun reduce(event: Event, previousState: ViewState): ViewState? {
+        when (event) {
+            is UIEvent.OnMovieClick -> {
+                TODO()
+            }
+            is UIEvent.GetMovies -> {
+                interactor.getMovies().fold(
+                    onSuccess = {
+                        processDataEvent(DataEvent.SuccessMoviesRequest(it))
+                    },
+                    onError = {
+                        processDataEvent(DataEvent.ErrorMoviesRequest(it.localizedMessage ?: ""))
+                    }
+                )
+            }
+
+            is DataEvent.SuccessMoviesRequest -> {
+                return previousState.copy(movies = event.movies, isLoad = false)
+            }
+
+            is DataEvent.ErrorMoviesRequest -> {
+                return previousState.copy(
+                    errorMessage = event.errorMessage
+                )
+            }
+        }
+
+        return null
+    }
+}
