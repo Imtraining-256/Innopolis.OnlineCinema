8c3d417b26e3b1408fd40cbbbd3230a21cc0f31a
atenyaeves <atenyaev@protonmail.com>
2021-11-23 22:05:14 +0700

    Deleted fragment

--- app/src/main/java/com/example/innopolisonlinecinema/MainActivity.kt
+++ app/src/main/java/com/example/innopolisonlinecinema/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.innopolisonlinecinema
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.example.innopolisonlinecinema.features.movies_list_screen.ui.MoviesListFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/innopolisonlinecinema/MoviesListFragment.kt
+++
@@ -1,5 +0,0 @@
-package com.example.innopolisonlinecinema
-
-import androidx.fragment.app.Fragment
-
-class MoviesListFragment : Fragment(R.layout.fragment_movies_list)
