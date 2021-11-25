1d2e820a46b99667905a93296aec3974307ca047
atenyaeves <atenyaev@protonmail.com>
2021-11-25 20:12:31 +0700

    Impl base view model

--- app/src/main/java/com/example/innopolisonlinecinema/features/movie_item_screen/MovieItemCardViewModel.kt
+++ app/src/main/java/com/example/innopolisonlinecinema/features/movie_item_screen/MovieItemCardViewModel.kt
@@ -1,3 +1,21 @@
 package com.example.innopolisonlinecinema.features.movie_item_screen
 
-class MovieItemCardViewModel
+import com.example.innopolisonlinecinema.base.BaseViewModel
+import com.example.innopolisonlinecinema.base.Event
+import com.example.innopolisonlinecinema.domain.model.MovieDomainModel
+
+class MovieItemCardViewModel(private val movie: MovieDomainModel) : BaseViewModel<ViewState>() {
+
+    override fun initialViewState(): ViewState {
+        return ViewState(movie)
+    }
+
+    override suspend fun reduce(event: Event, previousState: ViewState): ViewState? {
+        when (event) {
+            is UiEvent.OnPlayButtonListener -> {
+                processUiEvent(UiEvent.OnPlayButtonListener(event.movie))
+            }
+        }
+        return null
+    }
+}
