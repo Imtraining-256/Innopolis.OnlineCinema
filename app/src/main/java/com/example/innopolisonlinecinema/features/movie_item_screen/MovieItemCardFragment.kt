4a1a88f11af611a603336c2dc605cbca0fd30d8c
atenyaeves <atenyaev@protonmail.com>
2021-11-25 21:04:47 +0700

    Release Factory Method

--- app/src/main/java/com/example/innopolisonlinecinema/features/movie_item_screen/MovieItemCardFragment.kt
+++ app/src/main/java/com/example/innopolisonlinecinema/features/movie_item_screen/MovieItemCardFragment.kt
@@ -1,13 +1,24 @@
 package com.example.innopolisonlinecinema.features.movie_item_screen
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.innopolisonlinecinema.R
 import com.example.innopolisonlinecinema.databinding.FragmentMovieItemCardBinding
+import com.example.innopolisonlinecinema.domain.model.MovieDomainModel
 
 class MovieItemCardFragment : Fragment(R.layout.fragment_movie_item_card) {
     companion object {
-        fun newInstance() = MovieItemCardFragment()
+        fun newInstance(movie: MovieDomainModel): MovieItemCardFragment {
+            val args = Bundle()
+            args.apply {
+                putParcelable("movie", movie)
+            }
+
+            val fragment = MovieItemCardFragment()
+            fragment.arguments = args
+            return fragment
+        }
     }
 
     private val binding: FragmentMovieItemCardBinding by viewBinding(FragmentMovieItemCardBinding::bind)
