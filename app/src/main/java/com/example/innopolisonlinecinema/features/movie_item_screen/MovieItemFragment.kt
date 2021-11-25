f6e86af7f62ef41a69c416eac6c2d75f3c2050e6
atenyaeves <atenyaev@protonmail.com>
2021-11-25 17:24:28 +0700

    Added fragment

---
+++ app/src/main/java/com/example/innopolisonlinecinema/features/movie_item_screen/MovieItemFragment.kt
@@ -0,0 +1,6 @@
+package com.example.innopolisonlinecinema.features.movie_item_screen
+
+import androidx.fragment.app.Fragment
+import com.example.innopolisonlinecinema.R
+
+class MovieItemFragment : Fragment(R.layout.movie_item)
