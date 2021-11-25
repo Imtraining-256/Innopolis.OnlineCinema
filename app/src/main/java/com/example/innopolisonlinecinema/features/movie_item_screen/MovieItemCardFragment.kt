b1af467eba1441419ed9e1e669cfa088fadee3e8
atenyaeves <atenyaev@protonmail.com>
2021-11-25 23:41:08 +0700

    OnViewCreated

--- app/src/main/java/com/example/innopolisonlinecinema/features/movie_item_screen/MovieItemCardFragment.kt
+++ app/src/main/java/com/example/innopolisonlinecinema/features/movie_item_screen/MovieItemCardFragment.kt
@@ -1,25 +1,38 @@
 package com.example.innopolisonlinecinema.features.movie_item_screen
 
 import android.os.Bundle
+import android.view.View
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.innopolisonlinecinema.R
 import com.example.innopolisonlinecinema.databinding.FragmentMovieItemCardBinding
 import com.example.innopolisonlinecinema.domain.model.MovieDomainModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.koin.core.parameter.parametersOf
 
 class MovieItemCardFragment : Fragment(R.layout.fragment_movie_item_card) {
     companion object {
         fun newInstance(movie: MovieDomainModel): MovieItemCardFragment {
-            val args = Bundle()
-            args.apply {
-                putParcelable("movie", movie)
+            return MovieItemCardFragment().apply {
+                arguments = bundleOf(Pair("movie", movie))
             }
-
-            val fragment = MovieItemCardFragment()
-            fragment.arguments = args
-            return fragment
         }
     }
 
     private val binding: FragmentMovieItemCardBinding by viewBinding(FragmentMovieItemCardBinding::bind)
+    private val movie: MovieDomainModel by lazy {
+        requireArguments().getParcelable("movie")!!
+    }
+    private val viewModel by viewModel<MovieItemCardViewModel> { parametersOf(movie) }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            tvMovieTitle.text = movie.title
+            btnPlay.setOnClickListener {
+                viewModel.processUiEvent(UiEvent.OnPlayButtonListener(movie))
+            }
+        }
+    }
 }
