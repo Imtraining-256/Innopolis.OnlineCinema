72c340efd8e3472fca97fe039b81c456969e825a
atenyaeves <atenyaev@protonmail.com>
2021-11-28 00:43:16 +0700

    Added navigation via mainContainer

--- app/src/main/java/com/example/innopolisonlinecinema/MainActivity.kt
+++ app/src/main/java/com/example/innopolisonlinecinema/MainActivity.kt
@@ -3,15 +3,42 @@ package com.example.innopolisonlinecinema
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import com.example.innopolisonlinecinema.features.movies_list_screen.ui.MoviesListFragment
+import androidx.fragment.app.FragmentTransaction
+import com.example.innopolisonlinecinema.base.navigation.Screens
+import com.github.terrakok.cicerone.NavigatorHolder
+import com.github.terrakok.cicerone.Router
+import com.github.terrakok.cicerone.androidx.AppNavigator
+import com.github.terrakok.cicerone.androidx.FragmentScreen
+import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
+    private val navigatorHolder by inject<NavigatorHolder>()
+    private val navigator = object : AppNavigator(this, R.id.mainContainer) {
+        override fun setupFragmentTransaction(
+            screen: FragmentScreen,
+            fragmentTransaction: FragmentTransaction,
+            currentFragment: Fragment?,
+            nextFragment: Fragment
+        ) {
+            //setup your animation
+        }
+    }
+    private val router by inject<Router>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setFragment(MoviesListFragment())
+        setContentView(R.layout.activity_main)
+        navigatorHolder.setNavigator(navigator)
+        router.newRootScreen(Screens.moviesListScreen())
     }
 
-    private fun setFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction().replace(android.R.id.content, fragment).commit()
+    override fun onResumeFragments() {
+        super.onResumeFragments()
+        navigatorHolder.setNavigator(navigator)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        navigatorHolder.removeNavigator()
     }
 }
